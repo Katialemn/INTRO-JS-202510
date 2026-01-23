7fac4c713e67cafc7c85e1f92250e9c3ee2ea210
Katialemn <Katiuskahernandez2984@gmail.com>
2026-01-23 02:51:08 +0100

    Proyecto actualizado sin acentos

--- Desktop/INTRO-JS-202510/EjerciciosClase16/ejercicio8.js
+++
@@ -1,9 +0,0 @@
-/*Ejercicio 8: Busca un nombre específico 
-Tienes una lista de nombres ['Carlos', 'Daniel', 'Laura', 'Ana']. Queremos saber si el nombre 
-'Laura' está presente en la lista y, si lo está, deberías devolverlo. */
-
-let listaNombres = ["Carlos", "Daniel", "Laura", "Ana"];
-
-let encontrado = listaNombres.find(nombre => nombre === "Laura");
-
-console.log(encontrado);
